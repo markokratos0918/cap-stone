16b1baccce8c71dbd4819dec0ff76693e938ccfa
markokratos0918 <117650809+markokratos0918@users.noreply.github.com>
2023-03-01 23:07:23 +0800

    Added TopBanner, Background Image

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-
-
+import React from 'react';
+import TopBanner from './components/TopBanner/TopBanner';
 
 function App() {
   return (
     <div>
-      
+      <TopBanner />
     </div>
   );
 }
-
 export default App;
