75df34ae81be234c19f6536a3edbbb8386486352
markokratos0918 <117650809+markokratos0918@users.noreply.github.com>
2023-03-04 00:08:01 +0800

    Done styling navbar components

--- src/components/TopNavigation/TopNavigation.jsx
+++ src/components/TopNavigation/TopNavigation.jsx
@@ -2,6 +2,9 @@ import React, { Component, Fragment } from 'react'
 import { Navbar,Nav,Container } from 'react-bootstrap'
 import whiteLogo from '../../asset/image/logo_white.png';
 import blackLogo from '../../asset/image/logo_black.png';
+import '../../asset/css/custom.css';
+import '../../asset/css/bootstrap.min.css';
+
 
 class TopNavigation extends Component {
 
@@ -10,16 +13,18 @@ class TopNavigation extends Component {
         super();
         this.state={
              navBarTitle:"navTitle",  //css
-             navBarLogo: [whiteLogo] // object
+             navBarLogo: [whiteLogo], // object
+             navBarBack:"navBackground",
+             navBarItem:"navItem"
         }
    }
 
    onScroll=()=>{
         if(window.scrollY>100){
-             this.setState({navBarTitle:'navTitleScroll',navBarLogo:[blackLogo]})
+             this.setState({navBarTitle:'navTitleScroll',navBarLogo:[blackLogo],navBarBack:'navBackgroundScroll',navBarItem:'navItemScroll'})
              
         }else if(window.scrollY<100){
-             this.setState({navBarTitle:'navTitle',navBarLogo:[whiteLogo]})
+             this.setState({navBarTitle:'navTitle',navBarLogo:[whiteLogo],navBarBack:'navBackground',navBarItem:'navItem'})
         }
    }
 
@@ -32,7 +37,7 @@ class TopNavigation extends Component {
           return (
                 <Fragment>
 
-<Navbar collapseOnSelect fixed="top" expand="lg" bg="dark" variant="dark">
+<Navbar className={this.state.navBarBack} collapseOnSelect fixed="top" expand="lg" variant="dark">
       <Container>
         <Navbar.Brand className={this.state.navBarTitle} href="#home"><img src={this.state.navBarLogo} /></Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
@@ -41,12 +46,12 @@ class TopNavigation extends Component {
             
           </Nav>
           <Nav>
-          <Nav.Link href="#deets">HOME</Nav.Link>
-          <Nav.Link href="#deets">ABOUT</Nav.Link>
-          <Nav.Link href="#deets">SERVICE</Nav.Link>
-          <Nav.Link href="#deets">COURSES</Nav.Link>
-          <Nav.Link href="#deets">PORTFOLIO</Nav.Link>
-          <Nav.Link href="#deets">CONTACT US</Nav.Link>
+          <Nav.Link className={this.state.navBarItem} href="#deets">HOME</Nav.Link>
+          <Nav.Link className={this.state.navBarItem} href="#deets">ABOUT</Nav.Link>
+          <Nav.Link className={this.state.navBarItem}  href="#deets">SERVICE</Nav.Link>
+          <Nav.Link className={this.state.navBarItem} href="#deets">COURSES</Nav.Link>
+          <Nav.Link className={this.state.navBarItem} href="#deets">PORTFOLIO</Nav.Link>
+          <Nav.Link className={this.state.navBarItem} href="#deets">CONTACT US</Nav.Link>
 
             
           </Nav>
