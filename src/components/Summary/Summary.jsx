6ca37511851236609e6394300dda135dbe46e7f0
markokratos0918 <117650809+markokratos0918@users.noreply.github.com>
2023-03-07 22:12:59 +0800

    Added animation countup to summary section

--- src/components/Summary/Summary.jsx
+++ src/components/Summary/Summary.jsx
@@ -1,51 +1,117 @@
-import React, { Component, Fragment } from 'react'
-import { Col, Container, Row } from 'react-bootstrap'
-import '../../asset/css/bootstrap.min.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { Component, Fragment } from "react";
+import { Card, Col, Container, Row } from "react-bootstrap";
+import { faCheckSquare } from "@fortawesome/free-regular-svg-icons";
+import { faGlobe, faLaptop, faStar } from "@fortawesome/free-solid-svg-icons";
+import CountUp from "react-countup";
+import VisibilitySensor from "react-visibility-sensor";
 
 class Summary extends Component {
-     render() {
-          return (
-           <Fragment>
+	render() {
+		return (
+			<Fragment>
+				<Container fluid={true} className="summaryBanner p-0">
+					<div className="summaryBannerOverlay">
+						<Container className="text-center">
+							<Row>
+								<Col lg={8} md={6} sm={12}>
+									<Row className="countSection">
+										<Col>
+											<FontAwesomeIcon className="iconProject" icon={faGlobe} />
+											<h1 className="countNumber">
+												<CountUp start={0} end={35000}>
+													{({ countUpRef, start }) => (
+														<VisibilitySensor onChange={start} delayedCall>
+															<span ref={countUpRef} />
+														</VisibilitySensor>
+													)}
+												</CountUp>
+											</h1>
+											<h4 className="countTitle">Student Worldwide </h4>
+											<hr className="bg-white w-30" />
+										</Col>
 
-           <Container fluid={true} className="summaryBanner p-0" >
-             <div className="summaryBannerOverlay">
-               <Container className="text-center">
-                    <Row >
+										<Col>
+											<FontAwesomeIcon
+												className="iconProject"
+												icon={faLaptop}
+											/>
+											<h1 className="countNumber">
+												<CountUp start={0} end={22}>
+													{({ countUpRef, start }) => (
+														<VisibilitySensor onChange={start} delayedCall>
+															<span ref={countUpRef} />
+														</VisibilitySensor>
+													)}
+												</CountUp>
+											</h1>
+											<h4 className="countTitle">Courses Published</h4>
+											<hr className="bg-black w-30" />
+										</Col>
 
-                    <Col lg={8} md={6} sm={12}>
-                     <Row className="countSection">
-                        <Col>
-                            <h1 className="countNumber">35000 </h1>
-                            <h4 className="countTitle">Student Worldwide </h4>
-                            <hr className="bg-white w-25" />
-                        </Col>
+										<Col>
+											<FontAwesomeIcon className="iconProject" icon={faStar} />
+											<h1 className="countNumber">
+												<CountUp start={0} end={3000}>
+													{({ countUpRef, start }) => (
+														<VisibilitySensor onChange={start} delayedCall>
+															<span ref={countUpRef} />
+														</VisibilitySensor>
+													)}
+												</CountUp>
+											</h1>
+											<h4 className="countTitle">Student Review</h4>
+											<hr className="bg-white w-30" />
+										</Col>
+									</Row>
+								</Col>
 
-                        <Col>
-                            <h1 className="countNumber">22000</h1>
-                            <h4 className="countTitle">Courses Published</h4>
-                            <hr className="bg-black w-25" />
-                        </Col>
-
-                        <Col>
-                            <h1 className="countNumber">3000 </h1>
-                            <h4 className="countTitle">Student Review</h4>
-                            <hr className="bg-white w-25" />
-                        </Col>
-                      </Row>
-                     </Col>
-
-                        <Col lg={4} md={6} sm={12}>
-                            <h1>FOUR  </h1>
-                        </Col>
-                    </Row>
-
-               </Container> 
-                </div>
-             </Container>
-
-              </Fragment>
-          )
-     }
+								<Col lg={4} md={6} sm={12}>
+									<Card className="workCard">
+										<Card.Body>
+											<Card.Title className="cardTitle">
+												What We Have Achieved
+											</Card.Title>
+											<Card.Text>
+												<p className="cardSubTitle text-justify">
+													<FontAwesomeIcon
+														className="iconBullent"
+														icon={faCheckSquare}
+													/>{" "}
+													Requirment Gathering{" "}
+												</p>
+												<p className="cardSubTitle text-justify">
+													<FontAwesomeIcon
+														className="iconBullent"
+														icon={faCheckSquare}
+													/>{" "}
+													System Analysis{" "}
+												</p>
+												<p className="cardSubTitle text-justify">
+													<FontAwesomeIcon
+														className="iconBullent"
+														icon={faCheckSquare}
+													/>{" "}
+													Coding Testing{" "}
+												</p>
+												<p className="cardSubTitle text-justify">
+													<FontAwesomeIcon
+														className="iconBullent"
+														icon={faCheckSquare}
+													/>{" "}
+													Implementation{" "}
+												</p>
+											</Card.Text>
+										</Card.Body>
+									</Card>
+								</Col>
+							</Row>
+						</Container>
+					</div>
+				</Container>
+			</Fragment>
+		);
+	}
 }
 
-export default Summary
+export default Summary;
