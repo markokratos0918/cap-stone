5fe97ce461e8f099f51909858af6064421ca13e6
Gelo <gelocaindoy@gmail.com>
2023-03-13 15:38:18 +0800

    Update TopNavigation

--- src/components/TopNavigation/TopNavigation.jsx
+++ src/components/TopNavigation/TopNavigation.jsx
@@ -1,68 +1,140 @@
-import React, { Component, Fragment } from 'react';
-import { Navbar,Nav,Container } from 'react-bootstrap';
-import whiteLogo from '../../asset/image/logo_white.png';
-import blackLogo from '../../asset/image/logo_black.png';
-import '../../asset/css/custom.css';
-import '../../asset/css/bootstrap.min.css';
-
+import React, { Component, Fragment } from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { NavLink } from 'react-router-dom';
+import whiteLogo from "../../asset/image/logo_white.png";
+import blackLogo from "../../asset/image/logo_black.png";
+import "../../asset/css/custom.css";
+import "../../asset/css/bootstrap.min.css";
 
 class TopNavigation extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      navBarTitle: "navTitle", //css
+      navBarLogo: [whiteLogo], // object
+      navBarBack: "navBackground",
+      navVariant: "dark",
+      navBarItem: "navItem",
+      pageTitle: props.title,
+    };
+  }
 
-   
-    constructor(){
-        super();
-        this.state={
-             navBarTitle:"navTitle",  //css
-             navBarLogo: [whiteLogo], // object
-             navBarBack:"navBackground",
-             navVariant:"dark",
-             navBarItem:"navItem"
-        }
-   }
+  onScroll = () => {
+    if (window.scrollY > 100) {
+      this.setState({
+        navBarTitle: "navTitleScroll",
+        navBarLogo: [blackLogo],
+        navBarBack: "navBackgroundScroll",
+        navBarItem: "navItemScroll",
+        navVariant: "light",
+      });
+    } else if (window.scrollY < 100) {
+      this.setState({
+        navBarTitle: "navTitle",
+        navBarLogo: [whiteLogo],
+        navBarBack: "navBackground",
+        navBarItem: "navItem",
+        navVariant: "dark",
+      });
+    }
+  };
 
-   onScroll=()=>{
-        if(window.scrollY>100){
-          this.setState({navBarTitle:'navTitleScroll',navBarLogo:[blackLogo],navBarBack:'navBackgroundScroll',navBarItem:'navItemScroll',navVariant:'light'})
-             
-        }else if(window.scrollY<100){
-          this.setState({navBarTitle:'navTitle',navBarLogo:[whiteLogo],navBarBack:'navBackground',navBarItem:'navItem',navVariant:'dark'})
-        }
-   }
+  componentDidMount() {
+    window.addEventListener("scroll", this.onScroll);
+  }
 
-   componentDidMount(){
-    window.addEventListener('scroll',this.onScroll)
-}
-
-
-     render() {
-          return (
-                <Fragment>
-
-<Navbar className={this.state.navBarBack} collapseOnSelect fixed="top" expand="lg" variant={this.state.navVariant}>
-      <Container>
-        <Navbar.Brand className={this.state.navBarTitle} href="#home"><img src={this.state.navBarLogo} alt=""/></Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            
-          </Nav>
-          <Nav>
-          <Nav.Link className={this.state.navBarItem} href="#deets">HOME</Nav.Link>
-          <Nav.Link className={this.state.navBarItem} href="#deets">ABOUT</Nav.Link>
-          <Nav.Link className={this.state.navBarItem}  href="#deets">SERVICE</Nav.Link>
-          <Nav.Link className={this.state.navBarItem} href="#deets">COURSES</Nav.Link>
-          <Nav.Link className={this.state.navBarItem} href="#deets">PORTFOLIO</Nav.Link>
-          <Nav.Link className={this.state.navBarItem} href="#deets">CONTACT US</Nav.Link>
-
-            
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-
-                </Fragment>
-          )
-     }
+  render() {
+    return (
+      <Fragment>
+        <title>{this.state.pageTitle}</title>
+        <Navbar
+          className={this.state.navBarBack}
+          collapseOnSelect
+          fixed="top"
+          expand="lg"
+          variant={this.state.navVariant}
+        >
+          <Container>
+            <Navbar.Brand className={this.state.navBarTitle} href="#home">
+              <img src={this.state.navBarLogo} alt="" />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="me-auto"></Nav>
+              <Nav>
+                <Nav.Link>
+                  {" "}
+                  <NavLink
+                    exact
+                    activeStyle={{ color: "#ffd900" }}
+                    className={this.state.navBarItem}
+                    to="/"
+                  >
+                    HOME
+                  </NavLink>
+                </Nav.Link>
+                <Nav.Link>
+                  {" "}
+                  <NavLink
+                    exact
+                    activeStyle={{ color: "#ffd900" }}
+                    className={this.state.navBarItem}
+                    to="/about"
+                  >
+                    ABOUT
+                  </NavLink>{" "}
+                </Nav.Link>
+                <Nav.Link>
+                  {" "}
+                  <NavLink
+                    exact
+                    activeStyle={{ color: "#ffd900" }}
+                    className={this.state.navBarItem}
+                    to="/service"
+                  >
+                    SERVICE
+                  </NavLink>{" "}
+                </Nav.Link>
+                <Nav.Link>
+                  {" "}
+                  <NavLink
+                    exact
+                    activeStyle={{ color: "#ffd900" }}
+                    className={this.state.navBarItem}
+                    to="/course"
+                  >
+                    COURSES
+                  </NavLink>{" "}
+                </Nav.Link>
+                <Nav.Link>
+                  {" "}
+                  <NavLink
+                    exact
+                    activeStyle={{ color: "#ffd900" }}
+                    className={this.state.navBarItem}
+                    to="/porfolio"
+                  >
+                    PORTFOLIO
+                  </NavLink>{" "}
+                </Nav.Link>
+                <Nav.Link>
+                  {" "}
+                  <NavLink
+                    exact
+                    activeStyle={{ color: "#ffd900" }}
+                    className={this.state.navBarItem}
+                    to="/contact"
+                  >
+                    CONTACT US
+                  </NavLink>{" "}
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </Fragment>
+    );
+  }
 }
 
 export default TopNavigation;
