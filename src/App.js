57a800d1a28932a5f890771ff0132071b5ecf622
Gelo <gelocaindoy@gmail.com>
2023-03-13 13:55:15 +0800

    ServicePage + ContactPage

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import PageTop from './components/PageTop/PageTop';
 import TopNavigation from './components/TopNavigation/TopNavigation';
 import AllCoursePage from './pages/AllCoursePage';
 import PortfolioPage from './pages/PortfolioPage';
 import HomePage from './pages/HomePage';
+import ContactPage from './pages/ContactPage';
+import AllServicePage from './pages/AllServicePage';
 
 function App() {
   return (
     <div>
     <HomePage />
     <TopNavigation />
-    <PageTop pagetitle="About Me " />
     <AllCoursePage />
     <PortfolioPage />
+    <ContactPage />
+    <AllServicePage/>
     </div>
   );
 }
