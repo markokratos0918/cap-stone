c9d2149aae4b5027b34e9f3e4bbae1e62715a243
Gelo <gelocaindoy@gmail.com>
2023-03-13 11:33:09 +0800

    Formation Home Page

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
 import React from 'react';
-import TopBanner from './components/TopBanner/TopBanner';
-import Services from './components/Services/Services';
-import TopNavigation from './components/TopNavigation/TopNavigation';
-import Analysis from './components/Analysis/Analysis';
-import Summary from './components/Summary/Summary';
-import RecentProject from './components/RecentProject/RecentProject';
-import Courses from './components/Courses/Courses';
-import Video from './components/Video/Video';
-import ClientReView from './components/ClientReview/ClientReview';
-import Footer from './components/Footer/Footer';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <div>
-      <TopNavigation/>
-      <TopBanner />
-      <Services/>
-      <Analysis/>
-      <Summary/>
-      <RecentProject/>
-      <Courses /> 
-      <ClientReView />
-      <Video />
-      <Footer />
+    <HomePage />
     </div>
   );
 }
