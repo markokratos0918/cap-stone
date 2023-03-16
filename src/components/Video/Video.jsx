776295c18c518a9b350e1a30eba15d34c08955fe
init.defaultBranch <gelocaindoy@gmail.com>
2023-03-16 20:36:56 +0800

    Video API

--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -1,65 +1,97 @@
-import React, { Component, Fragment } from 'react'
-import { Button, Col, Container, Modal, Row } from 'react-bootstrap'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import{faVideoSlash} from '@fortawesome/free-solid-svg-icons'
-import 'video-react/dist/video-react.css'
-import { Player, BigPlayButton } from 'video-react'
+import React, { Component, Fragment } from "react";
+import { Col, Container, Modal, Row, Button } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faVideoSlash } from "@fortawesome/free-solid-svg-icons";
+import "video-react/dist/video-react.css";
+import ReactPlayer from "react-player";
+import RestClient from "../../RestAPI/RestClient";
+import AppUrl from "../../RestAPI/AppUrl";
+import ReactHtmlParser from "react-html-parser";
+import Loading from "../Loading/Loading";
+import Zoom from "react-reveal/Zoom";
 
-export class Video extends Component {
+class Video extends Component {
+  constructor() {
+    super();
+    this.state = {
+      show: false,
+      video_desciption: "",
+      video_url: "",
+      loading: true,
+    };
+  }
 
-    constructor(){
-        super();
-        this.state={
-            show:false
+  componentDidMount() {
+    RestClient.GetRequest(AppUrl.HomeVideo)
+      .then((result) => {
+        console.log(result);
+        if (result.length > 0) {
+          this.setState({
+            video_desciption: result[0]["video_desciption"],
+            video_url: result[0]["video_url"],
+            loading: false,
+          });
         }
-    }
+      })
+      .catch((error) => {
+        console.log(error);
+        this.setState({
+          loading: false,
+        });
+      });
+  }
 
-    modalClose=()=>this.setState({show:false})
-    modalOpen=()=>this.setState({show:true})
+  modalClose = () => this.setState({ show: false });
+  modalOpen = () => this.setState({ show: true });
 
   render() {
-    return (
-      <Fragment>
-        <Container className="text-center">
-        <h1 className='serviceMainTitle'>OUR VIDEOS</h1>
-            <div className='bottom'></div>
+    if (this.state.loading === true) {
+      return <Loading />;
+    } else {
+      return (
+        <Fragment>
+          <Container className="text-center">
+            <h1 className="serviceMainTitle">OUR VIDEOS</h1>
+            <div className="bottom"></div>
             <Row>
-                <Col lg={6} md={6} sm={12} className="videoText">
-                <p className='serviceDescription text-justify'>JMFG Starlink is an Internet Solution Provider that offers high-speed internet plans for both home and business use. With our innovative technology and reliable network, you can stay connected with ease, no matter where you are located.<br /><br />
+              <Col lg={6} md={6} sm={12} className="videText">
+                <Zoom top>
+                  <p className="serviceDescription text-justify">
+                    {ReactHtmlParser(this.state.video_desciption)}
+                  </p>{" "}
+                </Zoom>
+              </Col>
 
-One of the primary benefits of using JMFG Starlink is the speed of our internet service. Our advanced satellite technology provides fast, reliable internet with low latency, which means you can stream videos, download files, and browse the web with ease. This is particularly useful for businesses that rely on a stable internet connection for video conferencing, cloud-based applications, and other critical operations.<br /><br />
-
-To learn more about the benefits of using JMFG Starlink Internet Solution Provider for your home or business, watch this video. It provides an overview of our services and how we can help you stay connected.</p>
-                </Col>
-
-                <Col lg={6} md={6} sm={12} className="videoCard">
-                <FontAwesomeIcon onClick={this.modalOpen} className="iconProject" icon={faVideoSlash} />
-        
-                </Col>
+              <Col lg={6} md={6} sm={12} className="videoCard">
+                <FontAwesomeIcon
+                  onClick={this.modalOpen}
+                  className="iconProject"
+                  icon={faVideoSlash}
+                />
+              </Col>
             </Row>
-        </Container>
+          </Container>
 
-        <Modal size="lg" show={this.state.show} onHide={this.modalClose}>
-        
-        <Modal.Body>
-
-        <Player src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4">
-        <BigPlayButton position="center" />
-        </Player>
-
-
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={this.modalClose}>
-            Close
-          </Button>
-
-        </Modal.Footer>
-      </Modal>
-
-      </Fragment>
-    )
+          <Modal size="lg" show={this.state.show} onHide={this.modalClose}>
+            <Modal.Body>
+              <ReactPlayer
+                url={this.state.video_url}
+                width="100%"
+                height="100%"
+                controls={false}
+                playing={false}
+              ></ReactPlayer>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={this.modalClose}>
+                Close
+              </Button>
+            </Modal.Footer>
+          </Modal>
+        </Fragment>
+      );
+    } // end Else
   }
 }
 
-export default Video
+export default Video;
