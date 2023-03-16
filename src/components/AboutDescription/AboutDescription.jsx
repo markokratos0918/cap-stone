f2bee29fd27eb49eb07af8ec366fb31fdda6a82b
init.defaultBranch <gelocaindoy@gmail.com>
2023-03-16 19:57:12 +0800

    About Description API

--- src/components/AboutDescription/AboutDescription.jsx
+++ src/components/AboutDescription/AboutDescription.jsx
@@ -1,38 +1,57 @@
 import React, { Component, Fragment } from "react";
 import { Col, Container, Row } from "react-bootstrap";
+import RestClient from "../../RestAPI/RestClient";
+import AppUrl from "../../RestAPI/AppUrl";
+import ReactHtmlParser from "react-html-parser";
+import Loading from "../Loading/Loading";
+import WentWrong from "../WentWrong/WentWrong";
+import Zoom from "react-reveal/Zoom";
 
 class AboutDescription extends Component {
+  constructor() {
+    super();
+    this.state = {
+      aboutdesc: "...",
+      loading: true,
+      error: false,
+    };
+  }
+
+  componentDidMount() {
+    RestClient.GetRequest(AppUrl.Information)
+      .then((result) => {
+        if (result == null) {
+          this.setState({ error: true, loading: false });
+        } else {
+          this.setState({ aboutdesc: result[0]["about"], loading: false });
+        }
+      })
+      .catch(() => {
+        this.setState({ error: true });
+      });
+  }
+
   render() {
-    return (
-      <Fragment>
-        <Container className="mt-5">
-          <Row>
-            <Col sm={12} lg={12} md={12}>
-              <h1 className="serviceName">Who I Am </h1>
-              <hr />
-              <p className="serviceDescription">
-                Hi! I'm Name.
-                
-              </p>
-
-              <br></br>
-              <br></br>
-              <h1 className="serviceName">Our Mission </h1>
-              <hr />
-              <p className="serviceDescription">Mission
-              </p>
-
-              <br></br>
-              <br></br>
-              <h1 className="serviceName">Our Vision </h1>
-              <hr />
-              <p className="serviceDescription">Vision
-              </p>
-            </Col>
-          </Row>
-        </Container>
-      </Fragment>
-    );
+    if (this.state.loading === true) {
+      return <Loading />;
+    } else if (this.state.loading === false) {
+      return (
+        <>
+          <Fragment>
+            <Container className="mt-5">
+              <Row>
+                <Col sm={12} lg={12} md={12}>
+                  <Zoom top>{ReactHtmlParser(this.state.aboutdesc)}</Zoom>
+                </Col>
+              </Row>
+            </Container>
+          </Fragment>
+        </>
+      );
+    } // end Else
+    else if (this.state.error === true) {
+      return <WentWrong />;
+    } // end if for error
   }
 }
 
