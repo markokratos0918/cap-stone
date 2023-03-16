45b22f80e89368dd54cf362008ad591f93f7994c
init.defaultBranch <gelocaindoy@gmail.com>
2023-03-16 20:00:19 +0800

    Analysis API

--- src/components/Analysis/Analysis.jsx
+++ src/components/Analysis/Analysis.jsx
@@ -1,70 +1,88 @@
 import React, { Component, Fragment } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { Bar, BarChart, ResponsiveContainer, Tooltip, XAxis } from "recharts";
+import RestClient from "../../RestAPI/RestClient";
+import AppUrl from "../../RestAPI/AppUrl";
+import ReactHtmlParser from "react-html-parser";
+import Loading from "../Loading/Loading";
+import LightSpeed from "react-reveal/LightSpeed";
+import Zoom from "react-reveal/Zoom";
 
 class Analysis extends Component {
-  constructor() {
-    super();
-    this.state = {
-      data: [
-        { Technology: "PHP", Projects: 100 },
-        { Technology: "MySql", Projects: 90 },
-        { Technology: "Laravel", Projects: 95 },
-        { Technology: "React", Projects: 75 },
-        { Technology: "Opencart", Projects: 80 },
-        { Technology: "Vue Js", Projects: 50 },
-        { Technology: "Django", Projects: 60 },
-        { Technology: "JavaScript", Projects: 100 },
-      ],
-    };
-  }
+constructor() {
+super();
+this.state = {
+     data: [],
+     techdesc: "...",
+     loading: true,
+};
+}
 
-  render() {
-    var blue = "#051b35";
-    return (
-      <Fragment>
-        <Container className="text-center">
-          <h1 className="serviceMainTitle">TECHNOLOGY USED</h1>
+componentDidMount() {
+RestClient.GetRequest(AppUrl.ChartData).then((result) => {
+     // console.log(result);
+     this.setState({ data: result, loading: false });
+});
+// RestClient.GetRequest(AppUrl.HomeTechDesc).then((result) => {
+//      console.log(result);
+//      this.setState({ techdesc: result[0]['tech_description'] });
+// });
+RestClient.GetRequest(AppUrl.HomeTechDesc)
+  .then((result) => {
+    console.log(result);
+    if (result && result.length > 0 && result[0]['tech_description']) {
+      this.setState({ techdesc: result[0]['tech_description'] });
+    } else {
+      console.error("Error: Invalid data received from API.");
+    }
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
+
+}
+
+render() {
+if (this.state.loading === true) {
+     return <Loading />;
+} else {
+     var blue = "#051b35";
+     return (
+     <Fragment>
+          <Container className="text-center">
+          <LightSpeed top>
+          <h1 className="serviceMainTitle">COVERED AREAS</h1>
+          </LightSpeed>
           <div className="bottom"></div>
           <Row>
-            <Col
-              style={{ width: "100%", height: "300px" }}
-              lg={6}
-              md={12}
-              sm={12}
-            >
-              <ResponsiveContainer>
-                <BarChart width={100} height={300} data={this.state.data}>
-                  <XAxis dataKey="Technology" />
-                  <Tooltip />
-                  <Bar dataKey="Projects" fill={blue}></Bar>
-                </BarChart>
-              </ResponsiveContainer>
-            </Col>
+          <Col
+               style={{ width: "100%", height: "300px" }}
+               lg={6}
+               md={12}
+               sm={12}
+          >
+               <ResponsiveContainer>
+               <BarChart width={730} height={300} data={this.state.data}>
+               <XAxis dataKey="x_data" />
+               <Tooltip />
+               <Bar dataKey="y_data" fill={blue}></Bar>
+               </BarChart>
+               </ResponsiveContainer>
+          </Col>
 
-            <Col lg={6} md={12} sm={12}>
-              <p className="text-justify serviceDescription">
-                We are WD32P Group 4. We are web developers with a serious love
-                for teaching I am a founder of eLe JMFG Starlink Internet
-                Solution Provider and a passionate Web Developer, Programmer &
-                Instructor.<br></br>
-                <br></br>I am working online for the last 7 years and have
-                created several successful websites running on the internet. I
-                try to create a project-based course that helps you to learn
-                professionally and make you fell as a complete developer. easy
-                learning exists to help you succeed in life.<br></br>
-                <br></br>
-                Each course has been hand-tailored to teach a specific skill. I
-                hope you agree! Whether you’re trying to learn a new skill from
-                scratch or want to refresh your memory on something you’ve
-                learned in the past, you’ve come to the right place.
-              </p>
-            </Col>
+          <Col lg={6} md={12} sm={12}>
+               <Zoom top>
+               <p className="text-justify serviceDescription">
+                    {ReactHtmlParser(this.state.techdesc)}
+               </p>
+               </Zoom>
+          </Col>
           </Row>
-        </Container>
-      </Fragment>
-    );
-  }
+          </Container>
+     </Fragment>
+     );
+} // end Else
+}
 }
 
 export default Analysis;
