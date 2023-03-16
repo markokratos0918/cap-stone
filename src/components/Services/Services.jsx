bb33da97fbaff382729d44cb6b637a509412c56b
init.defaultBranch <gelocaindoy@gmail.com>
2023-03-16 20:24:57 +0800

    Services API

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,46 +1,60 @@
-import React, { Component, Fragment } from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
-import designIcon from '../../asset/image/design.png';
-import ecommerceIcon from '../../asset/image/ecommerce.png';
-import webIcon from '../../asset/image/web.png';
-
+import React, { Component, Fragment } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import RestClient from "../../RestAPI/RestClient";
+import AppUrl from "../../RestAPI/AppUrl";
+import Loading from "../Loading/Loading";
+import Zoom from "react-reveal/Zoom";
+import LightSpeed from "react-reveal/LightSpeed";
 class Services extends Component {
+  constructor() {
+    super();
+    this.state = {
+      myData: [],
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    RestClient.GetRequest(AppUrl.Services).then((result) => {
+      this.setState({ myData: result, loading: false });
+    });
+  }
+
   render() {
-    return (
+    if (this.state.loading === true) {
+      return <Loading />;
+    } else {
+      const MyList = this.state.myData;
+      const MyView =
+        MyList &&
+        MyList.map((MyList) => {
+          return (
+            <Col lg={4} md={6} sm={12}>
+              <Zoom top>
+                <div className="serviceCard text-center">
+                  <img className="ecommerceIcon" src={MyList.service_logo} />
+                  <h2 className="serviceName">{MyList.service_name}</h2>
+                  <p className="serviceDescription">
+                    {MyList.service_description}
+                  </p>
+                </div>
+              </Zoom>
+            </Col>
+          );
+        });
+
+      return (
         <Fragment>
-            <Container className="text-center">
-            <h1 className="serviceMainTitle">OUR SERVICES</h1>
-               <div className="bottom"></div>
-
-                   <Row>
-                  <Col lg={4} md={6} sm={12}>
-                  <div className="serviceCard text-center">
-                    <img className="ecommerceIcon" src={ecommerceIcon} alt=""/>
-                    <h2 className="serviceName">Ecommerce</h2>
-                  <p className="serviceDescription">We will design and develop ecommerce online store website.</p>
-                  </div>
-                  </Col>
-
-                  <Col lg={4} md={6} sm={12}>
-                  <div className="serviceCard text-center">
-                    <img className="designIcon" src={designIcon} alt=""/>
-                    <h2 className="serviceName">Ecommerce</h2>
-                    <p className="serviceDescription">We will design and develop ecommerce online store website.</p>
-                    </div>
-                  </Col>
-
-                  <Col lg={4} md={6} sm={12}>
-                    <div className="serviceCard text-center">
-                    <img className="webIcon" src={webIcon} alt=""/>
-                    <h2 className="serviceName">Web Development</h2>
-                    <p className="serviceDescription">Clean and fresh issue free code to make your website dynamic perfectly.</p>
-                  </div>
-                  </Col>
-                  </Row>
-            </Container>
+          <Container className="text-center">
+            <LightSpeed top>
+              <h1 className="serviceMainTitle">OUR SERVICES</h1>
+              <div className="bottom"></div>
+            </LightSpeed>
+            <Row>{MyView}</Row>
+          </Container>
         </Fragment>
-      
-    )
+      );
+    } // end Else
   }
 }
 
